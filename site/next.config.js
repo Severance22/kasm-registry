bda38aeaff3cb113cbea84618e9aedda079a5047
Severance22 <wesley.de.brandt@gmail.com>
2024-10-14 14:59:56 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,8 +2,8 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'Wes Technologiess',
+    description: 'The official store for Wes supported.',
     icon: '/img/logo.svg',
     listUrl: 'https://registry.kasmweb.com/',
     contactUrl: 'https://kasmweb.com/support',
